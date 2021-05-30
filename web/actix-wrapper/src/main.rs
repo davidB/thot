ffec70689acae2cc80b8d8e65fbfd208a4f20c6f
David Bernard <david.bernard.31@gmail.com>
2021-05-30 16:47:56 +0200

    :wrench: service should listen 0.0.0.0 to be usable from docker image

--- web/actix-wrapper/src/main.rs
+++ web/actix-wrapper/src/main.rs
@@ -47,7 +47,7 @@ async fn main() -> std::io::Result<()> {
     // env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
     env_logger::init();
     HttpServer::new(|| App::new().wrap(Logger::default()).configure(configure_app))
-        .bind("127.0.0.1:8080")?
+        .bind("0.0.0.0:8080")?
         .run()
         .await
 }
